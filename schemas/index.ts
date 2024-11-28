bff70ba094af15ab577447f4d804409310e580f0
Davide Avagnano <davideavagnano@MacBook-Air-di-Davide.local>
2024-11-28 16:11:17 +0100

    HomePage, LoginPage, RegisterPage, validation schemas login and register, frontend control and backend control with server actions login and register

---
+++ schemas/index.ts
@@ -0,0 +1,32 @@
+// Validation schemas for both frontend and backend using zod
+
+import * as z from "zod";
+
+/**
+ * Validation schema for Login Form:
+ * valid email address, non-empty password (allow old password rules)
+ */
+export const LoginSchema = z.object({
+  email: z.string().email({
+    message: "Email is required",
+  }),
+  password: z.string().min(1, {
+    message: "Password is required",
+  }),
+});
+
+/**
+ * Validation schema for Register Form:
+ * valid email address, password with at least 6 characters, non-empty name
+ */
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: "Email is required",
+  }),
+  password: z.string().min(6, {
+    message: "Minimum 6 characters required",
+  }),
+  name: z.string().min(1, {
+    message: "Name is required",
+  }),
+});
