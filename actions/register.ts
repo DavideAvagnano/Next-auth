b58586d97397f591ecfb3fc37fbd094fed879a4e
Davide Avagnano <davideavagnano@MacBook-Air-di-Davide.local>
2024-11-28 18:56:23 +0100

    Register functionality adding user to database

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,10 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcryptjs";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export async function register(values: z.infer<typeof RegisterSchema>) {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -10,5 +13,24 @@ export async function register(values: z.infer<typeof RegisterSchema>) {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Account created!" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
+
+  return { success: "User created!" };
 }
