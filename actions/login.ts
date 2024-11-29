116a9e0d7cd200e8aceb093fa8bb4fb4fc0bb993
Davide Avagnano <davideavagnano@MacBook-Air-di-Davide.local>
2024-11-29 08:52:56 +0100

    Credentials login functionality and logout

--- actions/login.ts
+++ actions/login.ts
@@ -2,6 +2,9 @@
 
 import * as z from "zod";
 import { LoginSchema } from "@/schemas";
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { AuthError } from "next-auth";
 
 export async function login(values: z.infer<typeof LoginSchema>) {
   const validatedFields = LoginSchema.safeParse(values);
@@ -10,5 +13,26 @@ export async function login(values: z.infer<typeof LoginSchema>) {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Account created!" };
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+
+    return { success: "Account created!" };
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials!" };
+        default:
+          return { error: "Something went wrong!" };
+      }
+    }
+
+    throw error;
+  }
 }
